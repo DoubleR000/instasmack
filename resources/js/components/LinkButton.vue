c620666462c0c19d37f8e69cbe6c96ada30c299d
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-08-29 11:42:47 +0800

    fix(login): add missing components

---
+++ resources/js/components/LinkButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <Link class="flex cursor-pointer flex-col items-center justify-center rounded-4xl text-center" :href="href">
+        <div>
+            <slot />
+        </div>
+    </Link>
+</template>
+<script lang="ts" setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    href: String,
+});
+</script>
---
+++ resources/js/components/TextInputField.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mb-4 rounded-2xl border border-gray-500 pr-5 pb-2 pl-5 hover:border-white">
+        <label class="text-sm text-gray-400"><slot /></label>
+        <input class="w-full select-none focus:outline-0" type="text" :name="name" :value="modelValue" @input="$emit('update:modelValue')" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    modelValue: String,
+    name: String,
+});
+</script>
