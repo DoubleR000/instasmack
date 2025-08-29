5c7b40ab89c6b4e7e74a033c3bd63e40361b5863
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-08-29 11:44:21 +0800

    style: add logo font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,12 +12,13 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=atma:300,400,500,600,700" rel="stylesheet" />
 
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         @inertia
     </body>
 </html>
