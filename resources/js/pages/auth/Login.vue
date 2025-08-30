859e3513242a01888ee39a88a05a936e17175e7f
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-08-30 10:36:42 +0800

    style(login): fixed responsiveness at very small screens

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,25 +1,29 @@
 <template>
     <AppLayout>
         <div class="md:grid md:h-screen md:grid-cols-12">
-            <div class="col-span-7 hidden flex-col items-center justify-center border-r-1 border-gray-600 md:flex">
-                <h1 class="logo font-extrabold md:text-7xl xl:text-9xl">InstaSmack</h1>
+            <div class="col-span-8 hidden flex-col items-center justify-center border-r-1 border-gray-600 md:flex">
+                <h1 class="logo font-extrabold md:text-7xl">InstaSmack</h1>
                 <div class="text-lg font-bold">See everyday moments from your close friends.</div>
             </div>
 
-            <div class="col-span-5 flex flex-col justify-center gap-5 p-10">
-                <h1 class="logo text-center text-2xl font-extrabold md:hidden">InstaSmack</h1>
-                <h1 class="text-left font-bold md:text-xl">Log into InstaSmack</h1>
-                <form @submit.prevent="form.post('/login')">
-                    <TextInputField v:model="form.email">Email</TextInputField>
-                    <TextInputField v:model="form.password">Password</TextInputField>
-                    <button class="h-12 w-full cursor-pointer rounded-4xl bg-blue-500 font-semibold hover:bg-blue-700" type="submit">Log In</button>
-                </form>
+            <div class="flex h-screen w-full flex-col justify-center overflow-x-auto text-lg md:col-span-4 md:items-center">
+                <div class="mx-auto w-125 gap-5 p-10 md:w-full">
+                    <h1 class="logo pb-4 text-center text-3xl font-extrabold md:hidden">InstaSmack</h1>
+                    <h1 class="pb-10 text-left font-bold md:text-xl">Log into InstaSmack</h1>
+                    <form @submit.prevent="form.post('/login')">
+                        <TextInputField v:model="form.email">Email</TextInputField>
+                        <TextInputField v:model="form.password">Password</TextInputField>
+                        <button class="mt-5 h-12 w-full cursor-pointer rounded-4xl bg-blue-500 font-semibold hover:bg-blue-700" type="submit">
+                            Log In
+                        </button>
+                    </form>
 
-                <LinkButton class="h-12 font-semibold hover:bg-gray-500">Forgot Password?</LinkButton>
+                    <LinkButton class="mt-3 h-12 font-semibold hover:bg-gray-500">Forgot Password?</LinkButton>
 
-                <LinkButton class="mt-10 h-12 font-semibold text-blue-400 ring-2 hover:bg-gray-500" :href="route('register')">
-                    Create a New Account
-                </LinkButton>
+                    <LinkButton class="mt-15 h-12 font-semibold text-blue-400 ring-2 hover:bg-gray-500" :href="route('register')">
+                        Create a New Account
+                    </LinkButton>
+                </div>
             </div>
         </div>
     </AppLayout>
