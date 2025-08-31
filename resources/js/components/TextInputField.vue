f5136606f9e788a0d7dc57f0e7b9eebe62c59b3a
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-08-31 19:04:10 +0800

    fix(ui): add password support to text input field component

--- resources/js/components/TextInputField.vue
+++ resources/js/components/TextInputField.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="mb-4 rounded-2xl border border-gray-500 pr-5 pb-2 pl-5 hover:border-white">
         <label class="text-sm text-gray-400"><slot /></label>
-        <input class="w-full select-none focus:outline-0" type="text" :name="name" :value="modelValue" @input="$emit('update:modelValue')" />
+        <input
+            class="h-12 w-full select-none focus:outline-0"
+            :type="type ?? 'text'"
+            :name="name"
+            :value="modelValue"
+            @input="$emit('update:modelValue')"
+        />
     </div>
 </template>
 
@@ -9,5 +15,6 @@
 defineProps({
     modelValue: String,
     name: String,
+    type: String,
 });
 </script>
