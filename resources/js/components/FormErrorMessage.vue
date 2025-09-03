b916c909a798df31682c7f5894c51669ed83c61f
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-09-03 20:34:11 +0800

    feat(ui): add support for multiple error messages to form error message

--- resources/js/components/FormErrorMessage.vue
+++ resources/js/components/FormErrorMessage.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="mb-2 text-sm text-red-400" v-if="fieldError"><circle-alert class="mr-1 inline pb-1" :size="24"></circle-alert>{{ fieldError }}</div>
+    <div class="mb-2 text-sm text-red-400" v-if="message">
+        <circle-alert class="mr-1 inline pb-1" :size="24" />
+        {{ message }}
+    </div>
 </template>
 
 <script setup lang="ts">
 import { CircleAlert } from 'lucide-vue-next';
+import { computed } from 'vue';
 
-defineProps({
-    fieldError: String,
+const props = defineProps({
+    fieldError: Array,
+});
+
+const message = computed(() => {
+    if (!props.fieldError) {
+        return undefined;
+    }
+
+    for (const item of props.fieldError) {
+        if (item !== undefined) {
+            return item;
+        }
+    }
+
+    return undefined;
 });
 </script>
